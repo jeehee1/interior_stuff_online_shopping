449994fb858ba21357b7f3553bd8e82f42670d64
jeehee1 <jeehee1w@gmail.com>
2023-03-24 16:54:29 +0000

    show image for dispaly stuff info

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ShowInterior from './components/ShowInterior';
+
+
+function App() {
+  return (
+    <div>
+      <ShowInterior/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ShowInterior.js
@@ -0,0 +1,12 @@
+const ShowInterior = () => {
+  return (
+    <div>
+      <img
+        src="https://images.unsplash.com/photo-1618220179428-22790b461013?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=327&q=80"
+        alt="cannot load the image."
+      />
+    </div>
+  );
+};
+
+export default ShowInterior;
