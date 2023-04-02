6b92ca96a6a2e42def751ba72c5aae0f001faa88
jeehee1 <jeehee1w@gmail.com>
2023-04-02 23:18:24 +0100

    make an error page component and set it as an errorElement in router

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import InteriorList from "./components/interiors/InteriorLists";
 import RootLayout from "./pages/Root";
 import ShowInteriors from "./components/interiors/ShowInteriors";
+import ErrorPage from "./pages/Error";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
+    errorElement: <ErrorPage />,
     id: "root",
     children: [
-      {index: true, element: <InteriorList/>},
+      { index: true, element: <InteriorList /> },
       { path: "/interiors", element: <InteriorList /> },
       { path: "/interiorsdetail", element: <ShowInteriors /> },
     ],
---
+++ src/pages/Error.js
@@ -0,0 +1,12 @@
+const ErrorPage = () => {
+  return (
+    <main>
+      <div>
+        <h1>Something went wrong...</h1>
+        <p>couldn't found page.</p>
+      </div>
+    </main>
+  );
+};
+
+export default ErrorPage;
