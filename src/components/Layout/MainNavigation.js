5202491b68aa933e0478ba5a2da4776ed46d9d55
jeehee1 <jeehee1w@gmail.com>
2023-04-04 15:26:24 +0100

    add list jsx in MainNavigation component

--- src/components/Layout/MainNavigation.js
+++ src/components/Layout/MainNavigation.js
@@ -4,11 +4,49 @@ import { NavLink } from "react-router-dom";
 const MainNavigation = () => {
   return (
     <header className={classes.header}>
-      <nav className={classes.nav}>
-        <NavLink to="/" className={({isActive}) => isActive? classes.active : undefined}>HOME</NavLink>
-        <NavLink to="/interiors" className={({isActive}) => isActive? classes.active : undefined}>INTERIORS</NavLink>
-        <NavLink to="/interiors/new" className={({isActive}) => isActive? classes.active : undefined}>REGISTER NEW INTERORS</NavLink>
-        <NavLink to="/interiors/details" className={({isActive}) => isActive? classes.active : undefined}>DETAILS</NavLink>
+      <nav>
+        <ul className={classes.nav}>
+          <li>
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+            >
+              HOME
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/interiors"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+            >
+              INTERIORS
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/interiors/new"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+            >
+              REGISTER NEW INTERORS
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/interiors/details"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+            >
+              DETAILS
+            </NavLink>
+          </li>
+        </ul>
       </nav>
     </header>
   );
