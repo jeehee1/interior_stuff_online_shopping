f190a6c363cccacc6b1b80c7b7ef5d0a5666406a
jeehee1 <jeehee1w@gmail.com>
2023-04-06 11:03:15 +0100

    change dummy data structure

--- src/components/interiors/ShowInteriors.js
+++ src/components/interiors/ShowInteriors.js
@@ -6,28 +6,6 @@ import { useParams } from "react-router-dom";
 import ShowInteriorsItems from "./ShowInteriorsDetail";
 import SideNavigation from "../layout/SideNavigation";
 
-const DUMMY_DATA = {
-  imgUrl:
-    "https://images.unsplash.com/photo-1618220179428-22790b461013?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=327&q=80",
-  items: [
-    {
-      name: "mirror",
-      price: 20,
-      desciption: "ordinary mirror nothing special",
-      shopAddress: "Somewhere",
-      coorX: 195,
-      coorY: 165,
-    },
-    {
-      name: "sofa",
-      price: 100,
-      desciption: "sofa that makes you comfortable",
-      shopAddress: "gotoparchase",
-      coorX: 213,
-      coorY: 422,
-    },
-  ],
-};
 
 const ShowInteriors = (props) => {
   const [showInfo, setShowInfo] = useState(false);
@@ -76,10 +54,10 @@ const ShowInteriors = (props) => {
     console.log("point x : " + event.pageX);
     console.log("point y : " + event.pageY);
     console.log("scroll y : " + window.pageYOffset);
-    console.log(y);
+    console.log(x, y);
   };
 
-  const interiorsInfo = props.info[0].info;
+  const interiorsInfo = props.info[0];
   console.log(interiorsInfo);
 
   const displayInfo = [];
